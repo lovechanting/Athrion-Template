cc69b424d113f8d8cfefac0212694286f624ec6c
mochi <124936830+lovechanting@users.noreply.github.com>
2025-03-18 14:07:31 +1400

    Create TagPatch.cs

---
+++ Patches/TagPatch.cs
@@ -0,0 +1,31 @@
+using HarmonyLib;
+using UnityEngine;
+
+public static class TagPatch
+{
+    public static void EST()
+    {
+        var harmony = new Harmony("com.yourmod.silenttag");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(VRRig), "CheckDistance")]
+    public class CheckDistancePatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true; 
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(VRRig), "CheckTagDistanceRollback")]
+    public class CheckTagDistanceRollbackPatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
